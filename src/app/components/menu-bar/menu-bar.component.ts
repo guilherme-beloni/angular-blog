dc52da77970bf2d133a31540bec7b73ccf4f957f
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-04-27 20:56:18 -0300

    Component_live

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
-
+  linkGithub: String = ""
+  linkLinkedin: String = ""
+  linkInstagram: String = ""
 }
